abf895de221faf099707841b14cae4ef2de285d2
daviddemirtas <davutdemirtas@gmail.com>
2020-02-29 12:00:58 -0500

    add ReserveRoom.java

---
+++ src/ReserveRoom.java
@@ -0,0 +1,15 @@
+public class ReserveRoom {
+    public static void main(String[] args) {
+
+      int[] arr={2, 3, 4, 5, 6} ;
+
+        for (int i = 0; i <arr.length ; i++) {
+            System.out.println(arr);
+        }
+
+        // only team lead can reserve
+
+
+
+    }
+}
--- src/Test/Logout.java
+++ src/Test/Logout.java
@@ -4,5 +4,12 @@ public class Logout {
     public static void main(String[] args) {
 
         System.out.println("welcome to selenium");
+
+        // updated new code
+
+        //hi
+
+
+
     }
 }
